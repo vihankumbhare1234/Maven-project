22c90ff21e0a82c1760c9b82e48e5f558e2ffa96
vihankumbhare1234 <130236043+vihankumbhare1234@users.noreply.github.com>
2025-08-28 15:50:41 +0530

    Create AppTest.java

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testMainMethod() {
+        // You can add assertions here to test the behavior of the App class
+        assertEquals(1, 1); // Placeholder assertion
+    }
+}
