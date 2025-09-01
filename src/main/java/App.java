ac90b1e30cd36c1be0b3b43c72b5e45b91a21379
vihankumbhare1234 <vihanak05@gmail.com>
2025-09-01 22:32:25 +0530

    Updated App-java print

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins Project!");
+        System.out.println("Hello World, Jenkins Project!");
     }
 }
